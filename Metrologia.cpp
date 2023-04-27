9a5fc31da32f6610a8731c32b69759e95d9ea637
Ohbtsr <126715240+Ohbtsr@users.noreply.github.com>
2023-04-27 12:26:20 -0300

    Ajuste Sem limite de valores
    Retirado Array

--- Metrologia.cpp
+++ Metrologia.cpp
@@ -1,4 +1,4 @@
-/*
+/* br Lindomar Baltasar
 Um(a) metrologista registra as medidas de um aparelho que variam entre -100 e
 100. Valores fora desse intervalo indicam um problema na leitura, que deve ser
 interrompida at ́e o aparelho ser recali- brado. Crie um programa que registra
@@ -6,49 +6,36 @@ essas leituras e imprime a m ́edia dos valores lidos. */
 
 #include <stdio.h>
 #include <stdlib.h>
+void calcularMedia();
 
-float numero[200]; // Vetor que armazenará as medidas
-float soma,media;
-int n;             // Número de medidas
-
-
-void calcularMedia(); // Função para cálculo da média
+float soma, media, medida;
+int n; // Número de medidas
 
 int main() {
   printf("Quantas medidas deseja armazenar? ");
   scanf("%d", &n);
 
-  if (n < 1 || n > 200) { // Verifica se o número de medidas é válido
-    printf("Valor inválido para a quantidade de medidas\n");
-    return 0;
-  }
-
   // Loop para inserção das medidas
   int i;
   for (i = 0; i < n; i++) {
     printf("Insira o %dº valor: ", i + 1);
-    scanf("%f", &numero[i]);
-    if (-100 <= numero[i] &&
-        numero[i] <= 100) { // Verifica se a medida é válida
-      soma += numero[i];    // Quando válida, é somada.
+    scanf("%f", &medida);
+    if (-100 <= medida && medida <= 100) { // Verifica se a medida é válida
+      soma += medida;
     } else {
       printf("Número digitado inválido.\n"); // Medida inválida.
       break;                                 // Interrompe novas medidas.
     }
   }
-  if (i == n) {      // Verifica se todas as medidas foram inseridas com sucesso
-    calcularMedia(); // Chama a função para calcular a média
-  }
-
+calcularMedia();
   return 0;
 }
 
 void calcularMedia() {
-   if (n >= 0) {
+  if (n >= 0) {
     media = soma / n;
     printf("Média dos valores armazenados: %.2f\n", media);
-  }
-  else {
+  } else {
     printf("Nenhum valor inserido.\n");
   }
 }
